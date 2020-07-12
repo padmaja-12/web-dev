a6e886385415c9c66b4d8916857ed09255fff26a
Padmaja Jha <jhajunu@gmail.com>
2020-07-12 20:41:12 +0530

    halfway through the project

--- movie-app/app.js
+++ movie-app/app.js
@@ -1,31 +1,86 @@
 var express = require("express");
 var app = express();
-
+var request = require("request");
+const fetch = require("node-fetch");
 
 app.set("view engine","ejs");
 
-var genre = [
-    {name : "Action" , img : "https://www.inviul.com/wp-content/uploads/2018/03/Actions-class-in-Selenium.jpg"},
-    {name : "Comedy" , img : "https://colchester-events.co.uk/wp-content/uploads/2018/09/comedystore_coloursplash_logo-1080x675.jpg"},
-    {name : "Drama" , img : "https://i0.wp.com/4.bp.blogspot.com/--EIQS3inAWs/V-MEcG_Op5I/AAAAAAAAdyk/Y6OrRRUqHYAEganAA6OCrQ7s_Qc1J_1jACLcB/s1600/drama.jpg"},
-    {name : "Fantasy" , img : "https://images.hdqwalls.com/wallpapers/colorful-fantasy-4k-z8.jpg"},
-    {name : "Horror" , img : "https://cdn.vox-cdn.com/thumbor/5vwNia3paNvsm1r68IBd03ooCLs=/0x0:3000x2000/1200x675/filters:focal(1260x760:1740x1240)/cdn.vox-cdn.com/uploads/chorus_image/image/61985015/horrorclassics_2.0.jpg"},
-    {name : "Mystery" , img : "https://melissabourbon.com/wp-content/uploads/2010/10/its-a-mystery-500x325.jpg"},
-    {name : "Romance" , img : "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/06/02/689090-relationship-02-thinkstock.jpg"},
-    {name : "Thriller" , img : "https://genylifestyles.com/wp-content/uploads/2019/09/GENY-banner-Thriller.jpg"},
-    {name : "Western" , img : "https://www.westernclassicmovies.com/wp-content/uploads/2017/08/tcm_john_wayne-1.jpg"}
-];
 
 
 app.get("/",(req,res) => {
     res.render("landing");
 });
 
-app.get("/generes",(req,res) => {
-     res.render("generes",{genre:genre});
+var post,movie;
+var favourites = [];
+app.get("/search", (req,res) => {
+     var url2 = "https://api.themoviedb.org/3/discover/movie?api_key=1510cae89a9c50b52ba4e6a2f5db4dbe&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=true&page="
+     fetch(url2+'60').then(function (response) {
+	if (response.ok) {
+		return response.json();
+	} else {
+		return Promise.reject(response);
+	}
+}).then(function (data) {
+
+	// Store the post data to a variable
+    post=data;
+
+	// Fetch another API
+	return fetch(url2+"125");
+
+}).then(function (response) {
+	if (response.ok) {
+		return response.json();
+	} else {
+		return Promise.reject(response);
+	}
+}).then(function (userData) {
+	res.render("search",{data:post, d:userData});
+}).catch(function (error) {
+	console.log(error);
 });
-
-
+})
+app.get("/result",(req,res)=>{
+    var query = req.query.search
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=1510cae89a9c50b52ba4e6a2f5db4dbe&query="+query
+      request(url,(error,response,body)=>{
+          if(!error && response.statusCode == 200){
+              var data = JSON.parse(body)
+              res.render("result",{data: data});
+          }
+      })
+});
+app.get("/main",(req,res)=>{
+    var id = req.query.id
+    var url1 = "https://api.themoviedb.org/3/movie/"+ id + "?api_key=1510cae89a9c50b52ba4e6a2f5db4dbe&append_to_response=videos,images,reviews"
+    var u = "https://api.themoviedb.org/3/movie/" + id + "/similar?api_key=1510cae89a9c50b52ba4e6a2f5db4dbe"
+    fetch(url1).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (data) {
+    
+        // Store the post data to a variable
+        movie=data;
+    
+        // Fetch another API
+        return fetch(u);
+    
+    }).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then(function (userData) {
+        res.render("main",{data:movie, d:userData});
+    }).catch(function (error) {
+        console.log(error);
+    });
+});
 app.listen(3000, function(){
     console.log("MovieApp sever is up....PORT: 3000");
 });
