833e11f0fd355f1d6d33d12c942b1c60fe214268
Padmaja Jha <jhajunu@gmail.com>
2020-07-13 18:12:07 +0530

    remove gitignore files

--- movie-app/app.js
+++ movie-app/app.js
@@ -4,6 +4,7 @@ var express = require("express"),
     fetch = require("node-fetch"),
     mongoose = require("mongoose"),
     passport = require("passport"),
+    flash = require("connect-flash"),
     bodyParser = require("body-parser"),
     User = require("./model/user"),
     LocalStrategy = require("passport-local"),
@@ -18,6 +19,7 @@ app.use(require("express-session")({
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -27,6 +29,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next) => {
     res.locals.currentUser = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
     next();
 })
 
@@ -66,7 +70,7 @@ app.get("/search",isLoggedIn, (req,res) => {
 	console.log(error);
 });
 })
-app.get("/search/result",(req,res)=>{
+app.get("/search/result",isLoggedIn,(req,res)=>{
     var query = req.query.search
     var url = "https://api.themoviedb.org/3/search/movie?api_key=1510cae89a9c50b52ba4e6a2f5db4dbe&query="+query
       request(url,(error,response,body)=>{
@@ -76,7 +80,7 @@ app.get("/search/result",(req,res)=>{
           }
       })
 });
-app.get("/search/main",(req,res)=>{
+app.get("/search/main",isLoggedIn,(req,res)=>{
     var id = req.query.id
     var url1 = "https://api.themoviedb.org/3/movie/"+ id + "?api_key=1510cae89a9c50b52ba4e6a2f5db4dbe&append_to_response=videos,images,reviews"
     var u = "https://api.themoviedb.org/3/movie/" + id + "/similar?api_key=1510cae89a9c50b52ba4e6a2f5db4dbe"
@@ -115,10 +119,11 @@ app.post("/register",(req,res)=> {
     var newUser = new User({username : req.body.username});
     User.register(newUser,req.body.password, (err,user) => {
         if(err){
-            console.log(err);
+            req.flash("error",err.message)
             return res.render("register");
         }
         passport.authenticate("local")(req,res, () => {
+            req.flash("success","Welcome to this page  "+ user.username)
             res.redirect("/search");
         });
     });
@@ -128,18 +133,21 @@ app.get("/login",(req,res) => {
 });
 
 app.post("/login",passport.authenticate("local",{
-    successRedirect : "/search",
+    successRedirect : "/search",   
     failureRedirect : "/login"
-}),(req,res) => {});
+}),(req,res) => {
+});
 
 app.get("/logout",(req,res) => {
     req.logout();
+    req.flash("success","You have been logged out!!")
     res.redirect("/");
 });
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
         return next();
     }
+    req.flash("error", "You Need To Login To Move Ahead!");
     res.redirect("/login");
 }
 app.listen(3000, function(){
---
+++ movie-app/public/function/function.js
@@ -0,0 +1,56 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myF() {
+    var x = document.getElementById("overview");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFun() {
+    var x = document.getElementById("video");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunc() {
+    var x = document.getElementById("pcom");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunct() {
+    var x = document.getElementById("review");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function mF() {
+    var x = document.getElementById("simmovie");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function mFun() {
+    var x = document.getElementById("image");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
